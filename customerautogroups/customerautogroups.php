bb0596b093c4e959d4a7ee2a1c6caae29e2bfb5d
herve <nenes_fr@yahoo.fr>
2016-06-18 17:07:50 +0200

    New branch dev

--- customerautogroups/customerautogroups.php
+++ customerautogroups/customerautogroups.php
@@ -32,7 +32,7 @@ class customerautogroups extends Module
         $this->author        = 'hhennes';
         $this->name          = 'customerautogroups';
         $this->tab           = 'hhennes';
-        $this->version       = '0.3.1';
+        $this->version       = '0.3.2';
         $this->need_instance = 0;
 
         parent::__construct();
